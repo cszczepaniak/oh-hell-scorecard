fa6f27f0228f34814b0effe2cb8e2c37ede0d2fe
cszczepaniak <cszczepaniak@gmail.com>
2020-08-05 16:21:55 -0500

    change the default test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,12 +4,12 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders oh hell', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/oh hell/i)).toBeInTheDocument();
 });
