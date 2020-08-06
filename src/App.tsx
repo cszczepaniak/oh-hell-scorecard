e534e3f48a3e0233e6b053a07da349b9d3a56f69
cszczepaniak <cszczepaniak@gmail.com>
2020-08-06 08:15:18 -0500

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import Home from './features/home/Home';
+import { Provider as BumbagProvider, ThemeConfig } from 'bumbag';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
-  return <div>oh hell</div>;
-}
+const theme: ThemeConfig = {
+  Icon: {
+    iconSets: [
+      {
+        icons: [faPlus, faMinus],
+        prefix: 'solid-',
+        type: 'font-awesome',
+      },
+      {
+        icons: [faPlus, faMinus],
+        prefix: 'regular-',
+        type: 'font-awesome',
+      },
+    ],
+  },
+};
+
+const App = () => {
+  return (
+    <BumbagProvider theme={theme}>
+      <Home />
+    </BumbagProvider>
+  );
+};
 
 export default App;
