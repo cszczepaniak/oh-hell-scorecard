24eb0a83fd37c60dad25ff0ded8c412ec162eeae
cszczepaniak <cszczepaniak@gmail.com>
2020-08-23 13:32:34 -0500

    tdst the nav button

---
+++ src/features/newGame/__test__/NavButton.test.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import { DisplayContext } from '../context';
+import { NavButton } from '../NavButton';
+
+const cleanupDOM = () => {
+  document.getElementsByTagName('html')[0].innerHTML = '';
+};
+
+test('button calls the correct event based on direction prop', () => {
+  const mockNext = jest.fn();
+  const mockPrev = jest.fn();
+
+  interface test {
+    direction: 'forward' | 'back';
+    expectedHandler: () => void;
+  }
+
+  const tests: test[] = [
+    {
+      direction: 'forward',
+      expectedHandler: mockNext,
+    },
+    {
+      direction: 'back',
+      expectedHandler: mockPrev,
+    },
+  ];
+  tests.forEach((t) => {
+    const { getByRole } = render(
+      <DisplayContext.Provider value={{ displayIdx: 0, next: mockNext, previous: mockPrev }}>
+        <NavButton direction={t.direction}>a</NavButton>
+      </DisplayContext.Provider>,
+    );
+    const btn = getByRole('button');
+    fireEvent.click(btn);
+    expect(t.expectedHandler).toHaveBeenCalled();
+    cleanupDOM();
+  });
+});
+
+test('button calls no event when the button is disabled', () => {
+  const mockNext = jest.fn();
+  const mockPrev = jest.fn();
+
+  interface test {
+    direction: 'forward' | 'back';
+  }
+
+  const tests: test[] = [
+    {
+      direction: 'forward',
+    },
+    {
+      direction: 'back',
+    },
+  ];
+  tests.forEach((t) => {
+    const { getByRole } = render(
+      <DisplayContext.Provider value={{ displayIdx: 0, next: mockNext, previous: mockPrev }}>
+        <NavButton direction={t.direction} disabled>
+          a
+        </NavButton>
+      </DisplayContext.Provider>,
+    );
+    const btn = getByRole('button');
+    fireEvent.click(btn);
+    expect(mockNext).not.toHaveBeenCalled();
+    expect(mockPrev).not.toHaveBeenCalled();
+    cleanupDOM();
+  });
+});
+
+test('button renders the correct children', () => {
+  const testText = 'abc';
+  const { getByText } = render(<NavButton direction='forward'>{testText}</NavButton>);
+  getByText(testText);
+});
