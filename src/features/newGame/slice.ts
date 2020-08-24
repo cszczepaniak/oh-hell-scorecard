bbc92e179de8bc5767c9c1a8b735c0c603181da8
cszczepaniak <cszczepaniak@gmail.com>
2020-08-23 20:48:22 -0500

    type and test the reducer

--- src/features/newGame/slice.ts
+++ src/features/newGame/slice.ts
@@ -11,29 +11,42 @@ export const initialState: INewGameState = {
   },
 };
 
+interface IAction {
+  type: string;
+}
+
+interface ISetPlayerNamesAction extends IAction {
+  payload: string[];
+}
+
+interface ISelectDealerAction extends IAction {
+  payload: string;
+}
+
+interface ISetScoringModeAction extends IAction {
+  payload: ScoringMode;
+}
+
 const newGameSlice = createSlice({
   name: 'newGame',
   initialState: initialState,
   reducers: {
-    setPlayerNames(state, action) {
+    setPlayerNames(state, action: ISetPlayerNamesAction) {
       state.playerNames = action.payload;
     },
-    selectDealer(state, action) {
-      if (!action.payload || !state.playerNames.includes(action.payload)) {
-        return;
+    selectDealer(state, action: ISelectDealerAction) {
+      if (!state.playerNames.includes(action.payload)) {
+        return state;
       }
       state.dealer = action.payload;
     },
     unselectDealer(state) {
       state.dealer = '';
     },
-    setSettings(state, action) {
-      state.settings = action.payload;
-    },
     toggleBonusRounds(state) {
       state.settings.bonusRounds = !state.settings.bonusRounds;
     },
-    setScoringMode(state, action) {
+    setScoringMode(state, action: ISetScoringModeAction) {
       state.settings.scoringMode = action.payload;
     },
   },
