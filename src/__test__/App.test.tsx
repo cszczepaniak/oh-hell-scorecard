198dc141486fc2234233874b6240fae8a85d3295
cszczepaniak <cszczepaniak@gmail.com>
2020-08-25 07:32:42 -0500

    test the app

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import App from '../App';
+
+// we have to set up a mock for window.matchMedia, otherwise we get an error
+// https://jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
+
+test('app renders home page', () => {
+  const { getByText } = render(<App />);
+  expect(getByText(/oh hell scorecard/i)).toBeInTheDocument();
+});
