34825865ea0dca9d1ebd830d6d8af1f4d0499ef7
cszczepaniak <cszczepaniak@gmail.com>
2020-08-25 08:08:54 -0500

    finish up testing the game

--- src/features/newGame/__test__/NewGame.test.tsx
+++ src/features/newGame/__test__/NewGame.test.tsx
@@ -5,23 +5,25 @@ import userEvent from '@testing-library/user-event';
 
 import { NewGame } from '../NewGame';
 
-test('should be able to click through the screens', async () => {
-  const { getAllByPlaceholderText, getAllByRole, getByText } = render(
+test('should be able to click through the screens and back', async () => {
+  const { getAllByPlaceholderText, getByText } = render(
     <NewGame minPlayers={3} maxPlayers={10} handleCreateGame={jest.fn()} />,
   );
   expect(getByText(/enter player names/i)).toBeInTheDocument();
   const playerNameInputs = getAllByPlaceholderText(/player \d+/i);
+  const names = ['a', 'b', 'c', 'd'];
   const selectDealerButton = getByText(/select dealer/i);
   for (let i = 0; i < playerNameInputs.length; i++) {
     await wait(async () => {
-      await userEvent.type(playerNameInputs[i], 'a');
+      await userEvent.type(playerNameInputs[i], names[i]);
     });
   }
   await wait(() => {
     fireEvent.click(selectDealerButton);
   });
   expect(getByText(/choose dealer/i)).toBeInTheDocument();
-  const chosenDealerButton = getAllByRole('button')[0];
+  const dealer = names[0];
+  const chosenDealerButton = getByText(dealer);
   const selectSettingsButton = getByText(/select settings/i);
   act(() => {
     fireEvent.click(chosenDealerButton);
@@ -30,4 +32,14 @@ test('should be able to click through the screens', async () => {
     fireEvent.click(selectSettingsButton);
   });
   expect(getByText(/choose settings/i)).toBeInTheDocument();
+
+  // now go backwards
+  act(() => {
+    fireEvent.click(getByText(/select dealer/i));
+  });
+  expect(getByText(/choose dealer/i)).toBeInTheDocument();
+  act(() => {
+    fireEvent.click(getByText(/player names/i));
+  });
+  expect(getByText(/enter player names/i)).toBeInTheDocument();
 });
