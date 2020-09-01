4ac4472656581e5d4865454d8906d31f84f0c8c1
cszczepaniak <cszczepaniak@gmail.com>
2020-08-31 22:07:35 -0500

    start to build out some types

---
+++ src/features/game/types.ts
@@ -0,0 +1,15 @@
+import { INewGameRequest } from '../../shared/newGame/types';
+
+export interface IGame extends INewGameRequest {
+  playerStats: { [name: string]: IGameStats };
+}
+
+export interface IGameStats {
+  score: number;
+  pointsPerRound: number;
+  bidsPerRound: number;
+  tricksPerRound: number;
+  totalBids: number;
+  hitPercentage: number;
+  pointsBack: number;
+}
