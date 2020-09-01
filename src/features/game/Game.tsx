a51adc100d8d35487bcf2348376f448349bf0637
cszczepaniak <cszczepaniak@gmail.com>
2020-08-31 21:56:33 -0500

    redirect if there's not a valid request

--- src/features/game/Game.tsx
+++ src/features/game/Game.tsx
@@ -1,9 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+
+import { useHistory } from 'react-router-dom';
 
 import { NewGameContext } from '../../shared/newGame/context';
+import { defaultRequest } from '../../shared/newGame/types';
 
 export const Game: React.FunctionComponent = () => {
   const { request: newGameRequest } = useContext(NewGameContext);
+  const history = useHistory();
 
-  return <div>{JSON.stringify(newGameRequest)}</div>;
+  useEffect(() => {
+    if (newGameRequest === defaultRequest) {
+      history.push('/');
+    }
+  }, [newGameRequest, history]);
+
+  return <div>{newGameRequest === defaultRequest ? '' : JSON.stringify(newGameRequest)}</div>;
 };
