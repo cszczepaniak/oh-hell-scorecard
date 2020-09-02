e73d4b3b925c74cebbfc14289cc27fc8f39f7a99
cszczepaniak <cszczepaniak@gmail.com>
2020-09-01 21:20:00 -0500

    test the game simplify game

--- src/features/game/Game.tsx
+++ src/features/game/Game.tsx
@@ -1,19 +1,16 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 
-import { useHistory } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 import { NewGameContext } from '../../shared/newGame/context';
 import { defaultRequest } from '../../shared/newGame/types';
 
 export const Game: React.FunctionComponent = () => {
   const { request: newGameRequest } = useContext(NewGameContext);
-  const history = useHistory();
 
-  useEffect(() => {
-    if (newGameRequest === defaultRequest) {
-      history.push('/');
-    }
-  }, [newGameRequest, history]);
-
-  return <div>{newGameRequest === defaultRequest ? '' : JSON.stringify(newGameRequest)}</div>;
+  return newGameRequest === defaultRequest ? (
+    <Redirect to='/' />
+  ) : (
+    <div>{newGameRequest === defaultRequest ? '' : JSON.stringify(newGameRequest)}</div>
+  );
 };
