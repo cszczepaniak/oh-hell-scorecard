d5ff817b2aa6df2965993cf917264c3403aeb8c0
cszczepaniak <cszczepaniak@gmail.com>
2020-09-09 07:44:46 -0500

    add some tests

---
+++ src/features/game/__test__/PlayerCard.test.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+
+import { render, screen, act, fireEvent } from '@testing-library/react';
+
+import { PlayerCard } from '../PlayerCard';
+
+test('player card displays player name', () => {
+  const name = 'bob';
+  render(
+    <PlayerCard
+      name={name}
+      dealerName=''
+      stats={{
+        bidsPerRound: 0,
+        currentBid: 0,
+        hitPercentage: 0,
+        pointsBack: 0,
+        pointsPerRound: 0,
+        score: 0,
+        totalBids: 0,
+        tricksPerRound: 0,
+      }}
+    />,
+  );
+  expect(screen.getByText(name)).toBeInTheDocument();
+});
+
+test('player card indicates if a player is dealing', () => {
+  const name = 'bob';
+  render(
+    <PlayerCard
+      name={name}
+      dealerName={name}
+      stats={{
+        bidsPerRound: 0,
+        currentBid: 0,
+        hitPercentage: 0,
+        pointsBack: 0,
+        pointsPerRound: 0,
+        score: 0,
+        totalBids: 0,
+        tricksPerRound: 0,
+      }}
+    />,
+  );
+  expect(screen.getByText(/dealer/i)).toBeInTheDocument();
+});
+
+test('player card displays score', () => {
+  const score = 123;
+  render(
+    <PlayerCard
+      name='testPlayer'
+      dealerName=''
+      stats={{
+        bidsPerRound: 0,
+        currentBid: 0,
+        hitPercentage: 0,
+        pointsBack: 0,
+        pointsPerRound: 0,
+        score: score,
+        totalBids: 0,
+        tricksPerRound: 0,
+      }}
+    />,
+  );
+  expect(screen.getByText(`${score}`)).toBeInTheDocument();
+});
+
+test('player card displays bid', () => {
+  const currentBid = 7;
+  render(
+    <PlayerCard
+      name='testPlayer'
+      dealerName=''
+      stats={{
+        bidsPerRound: 0,
+        currentBid: currentBid,
+        hitPercentage: 0,
+        pointsBack: 0,
+        pointsPerRound: 0,
+        score: 0,
+        totalBids: 0,
+        tricksPerRound: 0,
+      }}
+    />,
+  );
+  const regex = new RegExp(`current bid:\\s+${currentBid}`, 'i');
+  expect(screen.getByText(regex)).toBeInTheDocument();
+});
+
+test('stats panel is shown if the button is clicked', () => {
+  render(
+    <PlayerCard
+      name='testPlayer'
+      dealerName=''
+      stats={{
+        bidsPerRound: 0,
+        currentBid: 0,
+        hitPercentage: 0,
+        pointsBack: 0,
+        pointsPerRound: 0,
+        score: 0,
+        totalBids: 0,
+        tricksPerRound: 0,
+      }}
+    />,
+  );
+  const button = screen.getByText(/view stats/i);
+  act(() => {
+    fireEvent.click(button);
+  });
+  expect(screen.getByTestId('StatsPanel')).toBeInTheDocument();
+});
