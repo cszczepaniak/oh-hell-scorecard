2faa2ec3aa1101719c1e99d727632eeda6ab222d
cszczepaniak <cszczepaniak@gmail.com>
2020-09-09 07:20:44 -0500

    move the mocking of window.matchMedia to setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,3 +4,21 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
+
+// we have to set up a mock for window.matchMedia, otherwise we get an error
+// https://jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
