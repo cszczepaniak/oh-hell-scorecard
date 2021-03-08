1342a53ae7cd8a8dc09be3f963fc932367d8ea59
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-03-07 20:04:35 -0600

    add SPADeploy

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,8 +1,14 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { SPADeploy } from "cdk-spa-deploy";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new SPADeploy(this, id).createSiteWithCloudfront({
+      indexDoc: "index.html",
+      websiteFolder: "artifact",
+    });
 
     // The code that defines your stack goes here
   }
