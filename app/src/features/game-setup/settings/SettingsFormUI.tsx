5087f677ca0cced1db4fbe48809c5152b4c3d43e
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-03-26 23:46:11 -0500

    do the modal without material :)

--- app/src/features/game-setup/settings/SettingsFormUI.tsx
+++ app/src/features/game-setup/settings/SettingsFormUI.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import { Dialog } from '@material-ui/core';
-
+import { Modal } from '../../../components/shared/Modal';
 import { SettingsFormUIProps } from './SettingsForm';
 
 export const SettingsFormUI: React.FunctionComponent<SettingsFormUIProps> = ({
@@ -17,6 +16,20 @@ export const SettingsFormUI: React.FunctionComponent<SettingsFormUIProps> = ({
 }) => {
     return (
         <div className='flex flex-col items-center bg-white max-w-md mx-auto h-screen sm:h-full sm:my-8 p-4 sm:rounded-2xl sm:border sm:border-gray-200 sm:shadow-md'>
+            <Modal open={bonusRoundModalOpen} onClose={() => setBonusRoundModalOpen(false)}>
+                <h3 className='font-semibold text-lg'>Bonus Rounds</h3>
+                <p>
+                    Rounds with the most cards for the given number of players are bonus rounds. In a bonus round,
+                    players are awarded 20 points instead of 10 if they bid 0 and take 0 tricks.
+                </p>
+            </Modal>
+            <Modal open={scoringModeModalOpen} onClose={() => setScoringModeModalOpen(false)}>
+                <h3 className='font-semibold text-lg'>Scoring Mode</h3>
+                <p>
+                    Players are awarded 10 plus one point for each trick they take if they hit their bid, otherwise they
+                    receive negative points equal to the difference between bid and actual tricks taken.
+                </p>
+            </Modal>
             <h1 className='text-4xl mt-2 mb-4'>Game Settings</h1>
             <div className='flex flex-col space-y-4 w-full max-w-xs'>
                 <div className='flex justify-between items-center'>
@@ -37,15 +50,6 @@ export const SettingsFormUI: React.FunctionComponent<SettingsFormUIProps> = ({
                         ?
                     </button>
                 </div>
-                <Dialog open={bonusRoundModalOpen} onClose={() => setBonusRoundModalOpen(false)}>
-                    <div className='m-4'>
-                        <h3 className='font-semibold text-lg'>Bonus Rounds</h3>
-                        <p>
-                            Rounds with the most cards for the given number of players are bonus rounds. In a bonus
-                            round, players are awarded 20 points instead of 10 if they bid 0 and take 0 tricks.
-                        </p>
-                    </div>
-                </Dialog>
                 <div>
                     <p className='mb-1'>Scoring Mode</p>
                     <div className='flex justify-between items-center'>
@@ -61,16 +65,6 @@ export const SettingsFormUI: React.FunctionComponent<SettingsFormUIProps> = ({
                         </button>
                     </div>
                 </div>
-                <Dialog open={scoringModeModalOpen} onClose={() => setScoringModeModalOpen(false)}>
-                    <div className='m-4'>
-                        <h3 className='font-semibold text-lg'>Scoring Mode</h3>
-                        <p>
-                            Players are awarded 10 plus one point for each trick they take if they hit their bid,
-                            otherwise they receive negative points equal to the difference between bid and actual tricks
-                            taken.
-                        </p>
-                    </div>
-                </Dialog>
                 <button onClick={handleSubmit}>Start Game</button>
             </div>
         </div>
