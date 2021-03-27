1988a61da33f98901db91b604099309c650d3ac7
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-03-26 20:02:28 -0500

    a few UI tweaks

--- app/src/features/game-setup/settings/SettingsFormUI.tsx
+++ app/src/features/game-setup/settings/SettingsFormUI.tsx
@@ -38,28 +38,38 @@ export const SettingsFormUI: React.FunctionComponent<SettingsFormUIProps> = ({
                     </button>
                 </div>
                 <Dialog open={bonusRoundModalOpen} onClose={() => setBonusRoundModalOpen(false)}>
-                    <p className='m-4'>
-                        Rounds with the most cards for the given number of players are bonus rounds. In a bonus round,
-                        players are awarded 20 points instead of 10 if they bid 0 and take 0 tricks.
-                    </p>
+                    <div className='m-4'>
+                        <h3 className='font-semibold text-lg'>Bonus Rounds</h3>
+                        <p>
+                            Rounds with the most cards for the given number of players are bonus rounds. In a bonus
+                            round, players are awarded 20 points instead of 10 if they bid 0 and take 0 tricks.
+                        </p>
+                    </div>
                 </Dialog>
-                <div className='flex justify-between items-center'>
-                    <select onChange={handleScoringModeChange} value={scoringMode} className='flex-1'>
-                        <option value='Negative'>Negative</option>
-                        <option value='Standard'>Standard</option>
-                    </select>
-                    <button
-                        onClick={() => setScoringModeModalOpen(true)}
-                        className='ml-8 bg-black text-white font-medium rounded-full w-5 h-5 flex items-center justify-center'
-                    >
-                        ?
-                    </button>
+                <div>
+                    <p className='mb-1'>Scoring Mode</p>
+                    <div className='flex justify-between items-center'>
+                        <select onChange={handleScoringModeChange} value={scoringMode} className='flex-1'>
+                            <option value='Negative'>Negative</option>
+                            <option value='Standard'>Standard</option>
+                        </select>
+                        <button
+                            onClick={() => setScoringModeModalOpen(true)}
+                            className='ml-8 bg-black text-white font-medium rounded-full w-5 h-5 flex items-center justify-center'
+                        >
+                            ?
+                        </button>
+                    </div>
                 </div>
                 <Dialog open={scoringModeModalOpen} onClose={() => setScoringModeModalOpen(false)}>
-                    <p className='m-4'>
-                        Players are awarded 10 plus one point for each trick they take if they hit their bid, otherwise
-                        they receive negative points equal to the difference between bid and actual tricks taken.
-                    </p>
+                    <div className='m-4'>
+                        <h3 className='font-semibold text-lg'>Scoring Mode</h3>
+                        <p>
+                            Players are awarded 10 plus one point for each trick they take if they hit their bid,
+                            otherwise they receive negative points equal to the difference between bid and actual tricks
+                            taken.
+                        </p>
+                    </div>
                 </Dialog>
                 <button
                     onClick={handleSubmit}
