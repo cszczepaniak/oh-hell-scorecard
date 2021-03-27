222af42dbeb23346be3ebb3940a49161cf425b80
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-03-27 13:08:44 -0500

    clean up backgrounds

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FunctionComponent = () => {
     return (
         <Provider store={store}>
             <BrowserRouter>
-                <div className='max-w-3xl mx-auto'>
+                <div className='w-full h-screen sm:h-auto bg-white sm:bg-gray-50 sm:max-w-3xl sm:mx-auto'>
                     <Switch>
                         <Route exact path='/'>
                             <Home />
--- app/src/components/pages/Home.tsx
+++ app/src/components/pages/Home.tsx
@@ -5,7 +5,7 @@ import { useHistory } from 'react-router';
 export const Home: React.FunctionComponent = () => {
     const history = useHistory();
     return (
-        <div className='mx-auto my-8 text-center'>
+        <div className='mx-auto py-8 text-center'>
             <h1 className='text-3xl sm:text-5xl'>Oh Heck Scorecard</h1>
             <button onClick={() => history.push('/newGame')} className='mt-16 px-8 text-lg sm:text-xl'>
                 Start a Game
