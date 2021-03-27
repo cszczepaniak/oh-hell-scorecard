abfb4c0b2ee406b6b4eef54e0840c67beb926819
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-03-26 19:53:51 -0500

    spike out tailwind for the settings page; I like it!!

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { Container, createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
-import { grey } from '@material-ui/core/colors';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
@@ -11,39 +9,28 @@ import { SettingsForm } from './features/game-setup/settings/SettingsForm';
 import { Game } from './features/game/Game';
 import { store } from './redux/root';
 
-const theme = createMuiTheme({
-    palette: {
-        background: {
-            default: grey[100],
-        },
-    },
-});
-
 const App: React.FunctionComponent = () => {
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Container maxWidth='lg'>
-                        <Switch>
-                            <Route exact path='/'>
-                                <Home />
-                            </Route>
-                            <Route exact path='/newGame'>
-                                <PlayerNamesForm minPlayers={3} maxPlayers={10} />
-                            </Route>
-                            <Route exact path='/gameSettings'>
-                                <SettingsForm />
-                            </Route>
-                            <Route exact path='/game'>
-                                <Game />
-                            </Route>
-                        </Switch>
-                    </Container>
-                </BrowserRouter>
-            </Provider>
-        </ThemeProvider>
+        <Provider store={store}>
+            <BrowserRouter>
+                <div className='max-w-3xl mx-auto'>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Home />
+                        </Route>
+                        <Route exact path='/newGame'>
+                            <PlayerNamesForm minPlayers={3} maxPlayers={10} />
+                        </Route>
+                        <Route exact path='/gameSettings'>
+                            <SettingsForm />
+                        </Route>
+                        <Route exact path='/game'>
+                            <Game />
+                        </Route>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        </Provider>
     );
 };
 
---
+++ app/src/features/game-setup/settings/SettingsFormUI.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import { Dialog } from '@material-ui/core';
+
+import { SettingsFormUIProps } from './SettingsForm';
+
+export const SettingsFormUI: React.FunctionComponent<SettingsFormUIProps> = ({
+    bonusRoundModalOpen,
+    setBonusRoundModalOpen,
+    scoringModeModalOpen,
+    setScoringModeModalOpen,
+    bonusRounds,
+    setBonusRounds,
+    scoringMode,
+    handleScoringModeChange,
+    handleSubmit,
+}) => {
+    return (
+        <div className='flex flex-col items-center bg-white max-w-md mx-auto h-screen sm:h-full sm:my-8 p-4 sm:rounded-2xl sm:border sm:border-gray-200 sm:shadow-md'>
+            <h1 className='text-4xl mt-2 mb-4'>Game Settings</h1>
+            <div className='flex flex-col space-y-4 w-full max-w-xs'>
+                <div className='flex justify-between items-center'>
+                    <div className='flex-1'>
+                        <input
+                            id='bonus-rounds'
+                            type='checkbox'
+                            className='mr-2'
+                            checked={bonusRounds}
+                            onChange={() => setBonusRounds(!bonusRounds)}
+                        />
+                        <label htmlFor='bonus-rounds'>Bonus Rounds</label>
+                    </div>
+                    <button
+                        onClick={() => setBonusRoundModalOpen(true)}
+                        className='ml-8 bg-black text-white font-medium rounded-full w-5 h-5 flex items-center justify-center'
+                    >
+                        ?
+                    </button>
+                </div>
+                <Dialog open={bonusRoundModalOpen} onClose={() => setBonusRoundModalOpen(false)}>
+                    <p className='m-4'>
+                        Rounds with the most cards for the given number of players are bonus rounds. In a bonus round,
+                        players are awarded 20 points instead of 10 if they bid 0 and take 0 tricks.
+                    </p>
+                </Dialog>
+                <div className='flex justify-between items-center'>
+                    <select onChange={handleScoringModeChange} value={scoringMode} className='flex-1'>
+                        <option value='Negative'>Negative</option>
+                        <option value='Standard'>Standard</option>
+                    </select>
+                    <button
+                        onClick={() => setScoringModeModalOpen(true)}
+                        className='ml-8 bg-black text-white font-medium rounded-full w-5 h-5 flex items-center justify-center'
+                    >
+                        ?
+                    </button>
+                </div>
+                <Dialog open={scoringModeModalOpen} onClose={() => setScoringModeModalOpen(false)}>
+                    <p className='m-4'>
+                        Players are awarded 10 plus one point for each trick they take if they hit their bid, otherwise
+                        they receive negative points equal to the difference between bid and actual tricks taken.
+                    </p>
+                </Dialog>
+                <button
+                    onClick={handleSubmit}
+                    className='bg-blue-500 hover:bg-blue-600 p-2 border border-blue-700  text-white font-medium'
+                >
+                    Start Game
+                </button>
+            </div>
+        </div>
+    );
+};
