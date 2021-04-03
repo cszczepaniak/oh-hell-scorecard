49fc40de53ad20d244b3ac8955e05705dd24ee8e
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-04-02 22:51:18 -0500

    make IDs unique

--- app/src/shared/ui-components/TextInput.tsx
+++ app/src/shared/ui-components/TextInput.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { nanoid } from '@reduxjs/toolkit';
 import clsx from 'clsx';
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -7,12 +8,13 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const TextInput: React.FunctionComponent<Props> = ({ label, disabled, className, ...rest }) => {
+    const id = `${label}-${nanoid()}`;
     return (
         <div className='flex flex-col flex-1'>
-            <label htmlFor={label}>{label}</label>
+            <label htmlFor={id}>{label}</label>
             <input
                 {...rest}
-                id={label}
+                id={id}
                 type='text'
                 className={clsx(className, 'w-full', 'disabled:text-gray-400', 'disabled:bg-gray-100')}
                 aria-disabled={disabled}
